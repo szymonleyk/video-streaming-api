c50e0102b5f54f46b9429af5ee869f8f77f02490
Szymon Leyk <biuro@szymonleyk.pl>
2023-10-05 21:02:59 +0200

    #1 create empty spring boot project, inital commit

---
+++ src/main/java/pl/szymonleyk/videostreamingapi/VideoStreamingApiApplication.java
@@ -0,0 +1,13 @@
+package pl.szymonleyk.videostreamingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoStreamingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoStreamingApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/szymonleyk/videostreamingapi/VideoStreamingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.szymonleyk.videostreamingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoStreamingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
